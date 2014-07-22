7968599834da366c686454cf32bf15d8c5898042
Maria Famador <maria.famador87@gmail.com>
2014-07-22 15:44:28 +0800

    Modify importer2

--- importer2.rb
+++ importer2.rb
@@ -3,12 +3,10 @@ require 'bundler/setup'
 require 'coffee_randomizer_super_extreme'
 Bundler.setup
 
-module CoffeeRandomizerSuperExtreme
-  class TemplateRunner
-    puts "Enter Participant Count: (must be multiples of 3 i.e. 9 / 15/ 21 / 27 / 33 / etc.)"
-    count = gets.chomp
 
-    generator = CoffeeRandomizerSuperExtreme::TemplateGenerator.new(count.to_i)
-    generator.generate_groups
-  end
-end
+puts "Enter Participant Count: (must be multiples of 3 i.e. 9 / 15/ 21 / 27 / 33 / etc.)"
+count = gets.chomp
+
+generator = CoffeeRandomizerSuperExtreme::TemplateGenerator.new(count.to_i)
+generator.generate_groups
+  
