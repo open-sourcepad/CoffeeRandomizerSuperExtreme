249462603dfc4f9b46d3678093242c76648c40f8
Maria Famador <maria.famador87@gmail.com>
2014-07-22 15:42:29 +0800

    Rename importer.rb to importer2.rb for marias_work

---
+++ importer2.rb
@@ -0,0 +1,14 @@
+require 'rubygems' 
+require 'bundler/setup' 
+require 'coffee_randomizer_super_extreme'
+Bundler.setup
+
+module CoffeeRandomizerSuperExtreme
+  class TemplateRunner
+    puts "Enter Participant Count: (must be multiples of 3 i.e. 9 / 15/ 21 / 27 / 33 / etc.)"
+    count = gets.chomp
+
+    generator = CoffeeRandomizerSuperExtreme::TemplateGenerator.new(count.to_i)
+    generator.generate_groups
+  end
+end
